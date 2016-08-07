a01ccdc6612a0cf5e0204e7d687c12e96dc54b6b
kjnam <kyujnam@gmail.com>
2016-08-07 13:10:12 +0900

    feat: 상품기능구현

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author Kj Nam
+ * @since 2016-08-07
+ */
+@Service
+@Transactional
+public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
